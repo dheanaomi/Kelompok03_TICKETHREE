53d1867a6f203d47940fa5ae1c8242b88f5383a6
anisasulis16 <80144968+anisasulis16@users.noreply.github.com>
2021-06-18 10:54:32 +0700

    Update TICKETHREE Pemesanan Tiket Konser.py
    merapikan

--- TICKETHREE Pemesanan Tiket Konser.py
+++ TICKETHREE Pemesanan Tiket Konser.py
@@ -1,30 +1,41 @@
 import time
-import json
-import random
 localtime = time.asctime( time.localtime(time.time()) )
+'''
+melacak tanggal dan waktu pemesanan tiket
+'''
 
-# Memulai Program TICKETHREE
-# Menampilkan program
+import json
+'''
+database katalalog konser Tikethree
+'''
+
+import random
+'''
+mengasilkan angka acak sebagai kode unik tiket
+'''
+
+# Memulai program dengan mencetak tampilan awal 
 tampilan = 'Selamat Datang di Program TICKETHREE Aplikasi Pemesanan Tiket Konser Online'
 s = tampilan.center(120, "=")
 print(s)
 
+#katalag semua konser
 katalog_konser = {
     'Katalog_1': {'Nama Acara': 'We The Fest', 'Artis': 'Dipha Barus,Weird Genius,Roni & Joni,Cyda',
                   'Tanggal': 'Jumat, 11 Juni 2021', 'Kota': 'Medan',
                   'Tempat': 'Pendopo Rumah Dinas Gubernur Sumatra Utara Jln. Jendral Sudirman', 'Genre': 'EDM',
-                  'Harga': 1200000, 'Status\t': '>100'},
+                  'Harga': 1200000, 'Status\t': 150},
     'Katalog_2': {'Nama Acara': 'Indigo Fest', 'Artis': 'Fourtwenty,Burgerkill,Bottlesmoker,Mocca',
                   'Tanggal': 'Minggu, 20 Juni 2021', 'Kota': 'Riau',
                   'Tempat': 'Kompleks Bandar Seni Raja Ali Haji Purna', 'Genre': 'Indie', 'Harga': 1300000,
-                  'Status': '<100'},
+                  'Status': 98},
     'Katalog_3': {'Nama Acara': 'Java Jazz Fest', 'Artis': 'Tulus,Indra Lesmana,Maliq & DEssentials,Andien',
                   'Tanggal': 'Senin, 28 Juni 2021', 'Kota': 'Surabaya', 'Tempat': 'Tunjungan Plaza Convention Center',
-                  'Genre': 'Jazz', 'Harga': 1500000, 'Status': '<100'},
+                  'Genre': 'Jazz', 'Harga': 1500000, 'Status': 60},
     'Katalog_4': {'Nama Acara': 'Lalala Fest', 'Artis': 'Ardhito Pramono,Isyana Sarasvati,Yura Yunita,Crush',
                   'Tanggal': 'Sabtu, 5 Juni 2021', 'Kota': 'Jakarta',
                   'Tempat': 'Aula Simfonia 81 Jln. Industri Raya Blok B14', 'Genre': 'Pop', 'Harga': 500000,
-                  'Status': '>100'}
+                  'Status': 400}
 }
 
 tamp = """\n=================================================
